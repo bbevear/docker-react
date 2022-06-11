77ec925160045e635a772b533a89c8b836e70920
Brent Bevear <brent.a.bevear@gmail.com>
2022-06-11 12:58:44 +1000

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Wank
+          New wank
         </p>
         <a
           className="App-link"
